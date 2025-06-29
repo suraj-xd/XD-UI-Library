994ee9ea1058644c90472012043a8232d5ce4600
suraj-xd <unofficialartist3@gmail.com>
2025-06-29 23:35:22 +0530

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -53,12 +53,6 @@ function Footer() {
           </div>
           <div className="flex items-center gap-1 mt-2 md:mt-0">
             Ideated with
-            <a
-              href="https://v0.dev/chat/three-js-logo-converter-JEQ692TQD4t"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="font-medium text-primary hover:underline flex items-center transition-colors duration-200 gap-2"
-            >
               <svg
                 width="20"
                 height="20"
@@ -70,7 +64,6 @@ function Footer() {
                   d="M14.252 8.25h5.624c.088 0 .176.006.26.018l-5.87 5.87a1.889 1.889 0 01-.019-.265V8.25h-2.25v5.623a4.124 4.124 0 004.125 4.125h5.624v-2.25h-5.624c-.09 0-.179-.006-.265-.018l5.874-5.875a1.9 1.9 0 01.02.27v5.623H24v-5.624A4.124 4.124 0 0019.876 6h-5.624v2.25zM0 7.5v.006l7.686 9.788c.924 1.176 2.813.523 2.813-.973V7.5H8.25v6.87L2.856 7.5H0z"
                 ></path>
               </svg>
-            </a>
             <span className="text-muted-foreground">by</span>
             <a
               href="https://github.com/suraj-xd"
